4100100be126b19e80cde23eed3ed7c4718ef69c
cassio.szcampos@gmail.com <cassio.szcampos@gmail.com>
2022-02-20 11:48:02 -0300

    ajax1 ex2

--- trab7ajax/ex2/busca-endereco.php
+++ trab7ajax/ex2/busca-endereco.php
@@ -1,44 +1,44 @@
 <?php
 
+class Endereco
+{
+  public $rua;
+  public $bairro;
+  public $cidade;
+
+  function __construct($rua, $bairro, $cidade)
+  {
+    $this->rua = $rua;
+    $this->bairro = $bairro; 
+    $this->cidade = $cidade;
+  }
+}
+
 require "conexaoMysql.php";
 $pdo = mysqlConnect();
 
-$cep = $_GET['cep'] ?? '';
-console.error($cep);
+$cep = htmlspecialchars($_GET['cep']);
 
-$sql = <<<SQL
-    SELECT rua, bairro, cidade
-    FROM t7ex2_enderecos
-    WHERE cep = $cep
-    SQL;
+try {
 
-  try {
-    // $stmt = $pdo->prepare($sql);
-    // $stmt->execute([$email]);
-    $stmt->exec([$cep]);
-    $row = $stmt->fetch();
-  } 
-  catch (Exception $e) {
-    exit('Falha inesperada: ' . $e->getMessage());
-  }
-  
-  class Endereco
-  {
-    public $rua;
-    public $bairro;
-    public $cidade;
-    
-    function __construct($rua, $bairro, $cidade)
-    {
-      $this->rua = $row['rua'];
-      $this->bairro = $row['bairro'];
-      $this->cidade = $row['cidade'];
-    }
-  }
-  
-  $endereco = array_key_exists($cep, $enderecos) ? 
-  $enderecos[$cep] : new Endereco('', '', '');
-  
-  echo json_encode($endereco);
+  $sql = <<<SQL
+  SELECT rua, bairro, cidade
+  FROM t7ex2_enderecos
+  WHERE cep = '$cep'
+  SQL;
 
-?>
+  // Neste exemplo não é necessário utilizar prepared statements
+  // porque não há possibilidade de injeção de SQL, 
+  // pois nenhum parâmetro do usuário é utilizado na query SQL. 
+  // Além disso, como há resultados a serem processados, 
+  // utilizamos o método query (e não o exec).
+  $stmt = $pdo->query($sql);
+}
+catch (Exception $e) {
+  exit('Ocorreu uma falha: ' . $e->getMessage());
+}
+
+$row = $stmt->fetch();
+$endereco = new Endereco($row['rua'], $row['bairro'], $row['cidade']);
+
+echo json_encode($endereco);
